734cfcc9300295c8524f69f813b81d10ed4fae7b
Xiaobei <yibaiyilan@gmail.com>
2022-07-26 00:21:15 +0000

    update the images

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ import os
 
 ###### Set up variables
 list_of_choices=['banana', 'hug', 'kiss']
+list_of_images=['banana-minions.gif', 'hug-minions.gif','kiss-minions.gif']
 githublink = 'https://github.com/austinlasseter/chuck_norris_execution'
-image1='banana-minions.gif'
+image1='minions.jpg'
 heading1='What will A Monion Give You?'
 
 ########### Initiate the app
@@ -19,7 +20,7 @@ app.title='Minions!'
 ####### Layout of the app ########
 app.layout = html.Div([
     html.H2(heading1),
-    html.Img(src=app.get_asset_url(image1), style={'width': 'auto', 'height': '10%'}),
+    html.Img(id='your-image-output', src=app.get_asset_url(list_of_images[0]), style={'width': 'auto', 'height': '10%'}),
     dcc.Dropdown(id='your-input-here',
                 options=[{'label': i, 'value': i} for i in list_of_choices],
                 value='punch',
@@ -38,7 +39,10 @@ app.layout = html.Div([
 def display_value(whatever_you_chose):
     return f'The minion will now give you a {whatever_you_chose}.'
 
-
+@app.callback(dash.dependencies.Output('your-image-output', 'src'),
+              [dash.dependencies.Input('your-input-here', 'value')])
+def display_value(whatever_you_chose):
+    return app.get_asset_url(list_of_images[whatever_you_chose])
 ######### Run the app #########
 if __name__ == '__main__':
     app.run_server(debug=True)
